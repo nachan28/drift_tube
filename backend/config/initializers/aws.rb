3235fa4d45962046e0595fee46f8b0c9f7d581dd
nachan28 <torajiro282828@gmail.com>
2024-09-22 02:13:33 +0900

    新しいgemの追加・設定変更

--- backend/config/initializers/aws.rb
+++ backend/config/initializers/aws.rb
@@ -1,22 +1,22 @@
 # config/initializers/aws.rb
 
-require 'aws-sdk-sts'
+# require 'aws-sdk-sts'
 
-sts_client = Aws::STS::Client.new(
-  region: 'us-east-1'
-)
+# sts_client = Aws::STS::Client.new(
+#   region: 'us-east-1'
+# )
 
-assumed_role = sts_client.assume_role(
-  role_arn: 'arn:aws:iam::984877473294:role/nanako-account',
-  role_session_name: 'RailsCrossAccountSession'
-)
+# assumed_role = sts_client.assume_role(
+#   role_arn: 'arn:aws:iam::984877473294:role/nanako-account',
+#   role_session_name: 'RailsCrossAccountSession'
+# )
 
-# 一時的なクレデンシャルを取得
-Aws.config.update({
-  region: 'us-east-1',
-  credentials: Aws::Credentials.new(
-    assumed_role.credentials.access_key_id,
-    assumed_role.credentials.secret_access_key,
-    assumed_role.credentials.session_token
-  )
-})
+# # 一時的なクレデンシャルを取得
+# Aws.config.update({
+#   region: 'us-east-1',
+#   credentials: Aws::Credentials.new(
+#     assumed_role.credentials.access_key_id,
+#     assumed_role.credentials.secret_access_key,
+#     assumed_role.credentials.session_token
+#   )
+# })
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import Components from 'unplugin-vue-components/vite'
+import sass from 'sass'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -15,6 +16,13 @@ export default defineConfig({
       dts: true,
     }),
   ],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        implementation: sass,  // dart-sassを明示的に指定
+      },
+    },
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
